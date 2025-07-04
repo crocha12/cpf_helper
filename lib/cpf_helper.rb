0125cfc8e919a6926b5b2b9ce21e274c60dcf5c8
Camilo Rocha <camilorocha11@gmail.com>
2025-07-04 14:09:23 -0300

    feat: initial commit

---
+++ lib/cpf_helper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cpf_helper/version"
+
+module CpfHelper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cpf_helper_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CpfHelper do
+  it "has a version number" do
+    expect(CpfHelper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
